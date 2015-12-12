872cca27a6f74674ff936997951decbdfa39d055
Roman Iakobchuk <r.iakobchuk@gmail.com>
2015-12-12 11:17:36 +0200

    first component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
+import Article from './Article'
 
 class App extends Component {
     render() {
         return (
             <div>
                 <h1>Hello world!!</h1>
+                <Article />
             </div>
         )
     }
