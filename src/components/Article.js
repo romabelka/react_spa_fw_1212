1e13eb96cd19e71082e60c04d9693879f7ce59ca
Roman Iakobchuk <r.iakobchuk@gmail.com>
2015-12-12 11:20:02 +0200

    first component

---
+++ src/components/Article.js
@@ -0,0 +1,18 @@
+import React, {Component, PropTypes} from 'react'
+
+class Article extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Some title</h3>
+                <p>Some Text</p>
+            </div>
+        )
+    }
+}
+
+export default Article
